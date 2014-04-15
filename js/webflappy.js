f922441788e4b9a46360f4d541a7139b2f12f948
Daniel Francis <francis@sugarlabs.org>
2014-04-14 21:22:58 -0300

    Use object-oriented programming
    Signed-off-by: Daniel Francis <francis@sugarlabs.org>

--- js/webflappy.js
+++ js/webflappy.js
@@ -1,237 +1,252 @@
-var bgwidth = 963;
-
+var birdx = 100;
 var stage = new Kinetic.Stage({
   container: 'container',
   width: 700,
   height: 602
 });
 
-// Background
-var bg= new Kinetic.Layer();
-var bgimg = new Image();
-var bgobj1;
-var bgobj2;
-var bgx = 0;
-var loaded = 0;
+function Background() {
+  var self = this;
+  self.layer = new Kinetic.Layer();
+  self.image = new Image();
+  self.ready = false;
+  self.width = 963;
+  self.height = 602;
+  self.pos = 0;
 
-bgimg.onload = function() {
-  bgobj1 = new Kinetic.Image({
-    x: bgx,
-    y: 0,
-    image: bgimg,
-    width: bgwidth,
-    height: 602
-  });
-  bg.add(bgobj1);
-  bgobj2 = new Kinetic.Image({
-    x: bgx + bgwidth,
-    y: 0,
-    image: bgimg,
-    width: bgwidth,
-    height: 602
-  });
-  bg.add(bgobj2);
+  self.image.onload = function() {
+    self.shape1 = new Kinetic.Image({
+      x: self.pos,
+      y: 0,
+      image: self.image,
+      width: self.width,
+      height: self.height
+    });
+    self.layer.add(self.shape1);
+    self.shape2 = new Kinetic.Image({
+      x: self.pos + self.width,
+      y: 0,
+      image: self.image,
+      width: self.width,
+      height: self.height
+    });
+    self.layer.add(self.shape2);
 
-  loaded += 1;
-};
-bgimg.src = 'img/background.jpg';
-stage.add(bg);
+    self.ready = true;
+  };
 
-// Tubes
-var tubes= new Kinetic.Layer();
-tubeupimg = new Image();
-tubedownimg = new Image();
-var xdis = 300;
-var ydis = 150;
-var tubewidth = 60;
-var tubeheight = 275;
-var tubeup1, tubedown1;
-var tubex = 0;
-var tube1y = -275;
-var tube2y = -275;
-var tube3y = -275;
+  self.image.src = 'img/background.jpg';
+  stage.add(self.layer);
 
-tubeupimg.onload = function () {
-  tubeup1 = new Kinetic.Image({
-    x: tubex,
-    y: tube1y,
-    image: tubeupimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubeup1);
-  tubeup2 = new Kinetic.Image({
-    x: tubex + xdis,
-    y: tube2y,
-    image: tubeupimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubeup2);
-  tubeup3 = new Kinetic.Image({
-    x: tubex + xdis * 2,
-    y: tube3y,
-    image: tubeupimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubeup3);
-  tubes.draw()
-  loaded += 1;
+  self.update = function () {
+    self.pos -= 1
+    if (self.pos + self.width === 0) {
+      self.pos = 0;
+      self.shape1.move({x: self.width, y: 0});
+      self.shape2.move({x: self.width, y: 0});
+    } else {
+      self.shape1.move({x: -1, y: 0});
+      self.shape2.move({x: -1, y: 0});
+    }
+    self.layer.draw();
+  }
 }
 
-tubedownimg.onload = function (){
-  tubedown1 = new Kinetic.Image({
-    x: tubex,
-    y: tube1y + tubeheight * 2 + ydis,
-    image: tubedownimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubedown1);
-  tubedown2 = new Kinetic.Image({
-    x: tubex + xdis,
-    y: tube2y + tubeheight * 2 + ydis,
-    image: tubedownimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubedown2);
-  tubedown3 = new Kinetic.Image({
-    x: tubex + xdis * 2,
-    y: tube3y + tubeheight * 2 + ydis,
-    image: tubedownimg,
-    width: tubewidth,
-    height: tubeheight * 2
-  });
-  tubes.add(tubedown3);
-  tubes.draw()
-  loaded += 1;
-};
+function TubePair(x, distance, relation, onready) {
+  var self = this;
+  self.x = x + distance * relation;
+  self.y = -275;
+  self.ydis = 150;
+  self.width = 60;
+  self.height = 275;
 
-tubeupimg.src = 'img/tube1.png';
-tubedownimg.src = 'img/tube2.png';
+  self.actual_height = 275 * 2;
+  self.hiddenx = self.x + self.width;
 
-stage.add(tubes);
+  self.loadUp = function (img) {
+    self.upshape = new Kinetic.Image({
+      x: self.x,
+      y: self.y,
+      image: img,
+      width: self.width,
+      height: self.actual_height
+    });
+    onready(self.upshape);
+  }
 
-var bird = new Kinetic.Layer();
-birdimg = new Image()
-var birdy = 400;
-var up = false;
-var upcount = 0;
-birdimg.onload = function () {
-  birdobj = new Kinetic.Image({
-    x: 100,
-    y: birdy,
-    image: birdimg,
-    width: 60,
-    height: 60
-  });
-  bird.add(birdobj);
+  self.loadDown = function (img) {
+    self.downshape = new Kinetic.Image({
+      x: self.x,
+      y: self.y + self.actual_height + self.ydis,
+      image: img,
+      width: self.width,
+      height: self.actual_height
+    });
+    onready(self.downshape);
+  }
+
+  self.matches = function (y) {
+    if (birdx + 50 > self.x) {
+      var maxx = self.x + self.width;
+      if (birdx < maxx) {
+        var minY = self.y + self.actual_height - 10;
+        if (minY > y) {
+          return true;
+        }
+        var maxY = minY + self.ydis;
+        if (maxY < y) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  self.update = function () {
+    if (self.hiddenx === 0) {
+      var deltax = distance * 3;
+      var newy = Math.floor(Math.random() *
+                            (self.actual_height - self.ydis / 4)) -
+                            self.actual_height;
+      self.upshape.move({x: deltax, y: newy - self.y});
+      self.downshape.move({x: deltax, y: newy - self.y});
+      self.y = newy;
+      self.x += deltax;
+      self.hiddenx = self.x + self.width;
+    } else {
+      self.x--;
+      self.hiddenx = self.x + self.width;
+      self.upshape.move({x: -1, y: 0});
+      self.downshape.move({x: -1, y: 0});
+    }
+  }
 }
-birdimg.src = 'img/bird.png';
-stage.add(bird);
+
+function TubeEngine() {
+  var self = this;
+  self.layer = new Kinetic.Layer();
+  self.downimg = new Image();
+  self.upimg = new Image();
+  xdis = 300;
+  var tubes = [];
+  var loadedup = false;
+  var loadeddown = false;
+  self.ready = false;
+
+  for (var i = 0; i < 3; i++) {
+    tubes.push(new TubePair(0, xdis, i, function (shape) {
+      self.layer.add(shape);
+      self.layer.draw();
+    }));
+  }
+
+  self.upimg.onload = function () {
+    loadedup = true;
+    tubes.forEach(function (tube){
+      tube.loadUp(self.upimg);
+    });
+    if (loadeddown) {
+      self.ready = true;
+    }
+  }
+  self.downimg.onload = function () {
+    loadeddown = true
+    tubes.forEach(function (tube){
+      tube.loadDown(self.downimg);
+      console.log(tubes.length);
+    });
+    if (loadedup) {
+      self.ready = true;
+    }
+  }
+
+  self.upimg.src = 'img/tube1.png';
+  self.downimg.src = 'img/tube2.png';
+  stage.add(self.layer);
+
+  self.update = function () {
+    tubes.forEach(function (tube) {
+      tube.update();
+    });
+    self.layer.draw();
+  }
+
+  self.matchBird = function (y) {
+    for (var i = 0, l = tubes.length; i < l; i ++) {
+      var tube = tubes[i];
+      if (tube.matches(y)) {
+        console.log('matches')
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+function Bird () {
+  var self = this;
+  self.layer = new Kinetic.Layer();
+  self.image = new Image();
+  self.y = 400;
+  self.width = 60;
+  self.height = 60;
+  self.up = false;
+  self.upcount = 0;
+
+  self.image.onload = function () {
+    self.shape = new Kinetic.Image({
+      x: birdx,
+      y: self.y,
+      image: self.image,
+      width: self.width,
+      height: self.height
+    });
+    self.layer.add(self.shape);
+  }
+  stage.add(self.layer);
+  self.image.src = 'img/bird.png';
+
+  self.update = function () {
+    if (!self.up) {
+      if (self.upcount !== 0) {
+        self.y += 2
+        self.shape.move({x:0, y: 2});
+      } else {
+        self.y += 4;
+        self.shape.move({x:0, y: 4});
+        self.upcount--;
+      }
+    } else {
+      self.upcount++;
+      self.y -= 8;
+      self.shape.move({x: 0, y: -8});
+      if (self.upcount == 5) {
+        self.up = false;
+      }
+    }
+    self.layer.draw();
+  }
+}
+
+var background = new Background();
+var tubes = new TubeEngine();
+var bird = new Bird();
+
 
 // Animation
 setInterval(function () {
-  if (loaded == 3) {
-    bgx -= 1
-    if (bgx + bgwidth === 0) {
-      bgx = 0;
-      bgobj1.move({x: bgwidth, y: 0});
-      bgobj2.move({x: bgwidth, y: 0});
-    } else {
-      bgobj1.move({x: -1, y: 0});
-      bgobj2.move({x: -1, y: 0});
-    }
-    bg.draw();
-    if (tubex + tubewidth === 0) {
-      tubex = tubex + xdis * 3;
-      var newy = Math.floor(Math.random() * (tubeheight * 2 - ydis / 4)) - tubeheight * 2;
-      console.log(newy);
-      tubeup1.move({x: xdis * 3, y: newy - tube1y});
-      tubedown1.move({x: xdis * 3, y: newy - tube1y});
-      tube1y = newy;
-      tubeup2.move({x: -1, y: 0});
-      tubedown2.move({x: -1, y: 0});
-      tubeup3.move({x: -1, y: 0});
-      tubedown3.move({x: -1, y: 0});
-    } else if (tubex - xdis * 2 + tubewidth === 0) {
-      tubex--;
-      var newy = Math.floor(Math.random() * (tubeheight * 2 - ydis / 4)) - tubeheight * 2;
-      tubeup2.move({x: xdis * 3, y: newy - tube2y});
-      tubedown2.move({x: xdis * 3, y: newy - tube2y});
-      tube2y = newy;
-      tubeup1.move({x: -1, y: 0});
-      tubedown1.move({x: -1, y: 0});
-      tubeup3.move({x: -1, y: 0});
-      tubedown3.move({x: -1, y: 0});
-    } else if (tubex - xdis + tubewidth == 0){
-      tubex--;
-      var newy = Math.floor(Math.random() * (tubeheight * 2 - ydis / 4)) - tubeheight * 2;
-      tubeup3.move({x: xdis * 3, y: newy - tube3y});
-      tubedown3.move({x: xdis * 3, y: newy - tube3y});
-      tube3y = newy;
-      tubeup1.move({x: -1, y: 0});
-      tubedown1.move({x: -1, y: 0});
-      tubeup2.move({x: -1, y: 0});
-      tubedown2.move({x: -1, y: 0});
-    } else {
-      tubex--;
-      tubeup1.move({x: -1, y: 0});
-      tubedown1.move({x: -1, y: 0});
-      tubeup2.move({x: -1, y: 0});
-      tubedown2.move({x: -1, y: 0});
-      tubeup3.move({x: -1, y: 0});
-      tubedown3.move({x: -1, y: 0});
-    }
-    tubes.draw();
-    if (!up) {
-      if (upcount !== 0) {
-        birdy += 2
-        birdobj.move({x:0, y: 2});
-      } else {
-        birdy += 4;
-        birdobj.move({x:0, y: 4});
-        upcount--;
-      }
-    } else {
-      upcount++;
-      birdy -= 8;
-      birdobj.move({x: 0, y: -8});
-      if (upcount == 5) {
-        up =false;
-      }
-    }
-    bird.draw();
+  if (background.ready && tubes.ready) {
+    background.update();
+    tubes.update();
+    bird.update()
 
-    var tube2 = tubex - xdis * 2;
-    var tube3 = tubex - xdis;
-    if (tubex > 95 - tubewidth && tubex < 155) {
-      var min = tube1y + tubeheight * 2;
-      var max = min + ydis;
-      if (birdy < min || birdy > max) {
-        alert('Game over, press F5 to start again');
-      }
-    }
-    else if (tube2 > 95 - tubewidth && tube2 < 155 ) {
-      var min = tube2y + tubeheight * 2;
-      var max = min + ydis;
-      if (birdy < min || birdy + 60 > max) {
-        alert('Game over, press F5 to start again');
-      }
-    }
-    else if (tube3 > 95 - tubewidth && tube3 < 155) {
-      var min = tube3y + tubeheight * 2;
-      var max = min + ydis;
-      if (birdy < min || birdy > max) {
-        alert('Game over, press F5 to start again');
-      }
+    if (tubes.matchBird(bird.y)){
+      alert('Game over, press F5 to start again');
     }
   }
 }, 0);
 
 window.onkeypress = function () {
-  up = true;
-  upcount = 0;
+  bird.up = true;
+  bird.upcount = 0;
 }
